6e6d2fa6f23941a2d7a20b5bf87d5b32612053e7
Elias Rosales <eliasrosales88@gmgmail.com>
2019-07-22 21:49:59 +0200

    Testing routes. Validation still work in progress

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -2,10 +2,10 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require('morgan'); //liberia para hacer logs
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var expressValidator = require('express-validator');
+
 
 var app = express();
 
@@ -13,14 +13,35 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Middlewares ---> una funcion que recibe 3 parametros
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); //en postman para procesar peticiones tipo form-data necesita un middleware i.e multerjs
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));// Otro middleware que sirve para servir ficheros estaticos
+// app.use('/pdf', express.static(pat.join(__dirname, 'd:pdf')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use(( req, res, next)=> {
+  // Un middleware tiene que hacer una de 2 cosas:
+
+  //  - Responder
+  // res.send('ok');
+
+  //  - O llamar a next
+  // console.log('Peticion a', req.originalUrl);
+  
+  next();
+
+})
+
+
+/* 
+ * Rutas de mi aplicacion
+ */
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,6 +50,11 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+if (err.array) {
+  const errInfo = err.array({onlyFirstError: true})[0];
+  err.message = `Not valid - ${errInfo.param} ${errInfo.msg}`;
+}
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- nodeapi/routes/index.js
+++ nodeapi/routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+//object destructuring
+// const { query, validationResult } = require('express/validator/check');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/paramenruta/:numero?', (req, res, next) => {
+  console.log('req.params',req.params);
+  res.send('ok');
+  
+});
+
+// Parametros predefinidos
+router.get('/params/:id([0-9]+)/piso/:piso/puerta/:puerta', (req, res, next) => {
+  console.log('req.params',req.params);
+  res.send('ok');
+  
+});
+
+
+// // Parametros en querystring
+// router.get('/enquerystring', query('color').isLowercase().withMessage('must be lower case')
+//   , (req, res, next) => {
+//     validationResult(req).throw();//lanza excepcion si no valida
+//     //si llego aqui es que los parametros de entrada son validos
+//     console.log('req.query',req.query);
+//   res.send('ok');
+  
+// });
+
+// Recibiendo peticion post
+router.post('/rutapost', (req, res, next) => {
+  console.log('req.body',req.body);
+  res.send('ok');
+  
+});
+
 
 module.exports = router;
